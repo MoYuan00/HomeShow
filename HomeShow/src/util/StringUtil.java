9ccaba96729e98a128722e1869d95e737541c872
geekjoker-joy <geekjoker@foxmail.com>
2019-12-23 02:22:19 +0800

    删除原先多余的HomeShow文件夹

--- HomeShow/src/util/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import java.io.UnsupportedEncodingException;
-
-public class StringUtil {
-    public static String getNewString(String str) throws UnsupportedEncodingException {
-        return new String(str.getBytes("ISO-8859-1"),"UTF-8");
-    }
-}
